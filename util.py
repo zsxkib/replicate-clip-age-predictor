cd529d457cd9d77b3adbb905a212c264d26edfe5
Sakib Ahamed <raptor5600@gmail.com>
2023-06-06 16:26:16 -0600

    Delete util.py

--- util.py
+++
@@ -1,8 +0,0 @@
-def compute_similarity(image_features, prompt_features):
-    return (
-        (100.0 * image_features @ prompt_features.T)
-        .softmax(dim=-1)
-        .detach()
-        .cpu()
-        .numpy()
-    )
